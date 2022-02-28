dccb6d389d9b5da2ffe0167a1e4f1fa5adf44141
chsdwn <chasedown@hotmail.com>
2022-02-28 18:03:40 +0300

    Installs mongoose. Connects the database. Fixes the post form's input fields. Creates the post endpoint. Renders saved posts on index page.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Post = require('./models/post')
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', { posts })
 })
 app.get('/about', (req, res) => {
   res.render('about')
@@ -16,6 +24,10 @@ app.get('/add', (req, res) => {
 })
 app.get('/add/:id', (req, res) => {
   res.render('post')
+})
+app.post('/post', async (req, res) => {
+  await Post.create(req.body)
+  res.redirect('/')
 })
 
 const port = 5000
