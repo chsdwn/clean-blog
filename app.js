75e4a0f02321e8ba9ea88db744db33c1063deafc
chsdwn <chasedown@hotmail.com>
2022-02-28 17:00:49 +0300

    Installs and enables ejs template engine. Enables static files. Creates views and public files.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' }
-  res.send(blog)
+  res.render('index')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+app.get('/add', (req, res) => {
+  res.render('add_post')
+})
+app.get('/add/:id', (req, res) => {
+  res.render('post')
 })
 
 const port = 5000
